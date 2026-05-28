d3140c5b9693a5fb6e78a75363e93524f354f703
ukkoh-h <ukkohh@gmail.com>
2026-05-28 09:57:59 +0300

    Goblin can deal dmg

--- Assets/Scripts/Enemy/HitBox.cs
+++ Assets/Scripts/Enemy/HitBox.cs
@@ -4,6 +4,7 @@ public class HitBox : MonoBehaviour
 {
     [SerializeField] private bool isHead;
     [SerializeField] private Enemy goblin;
+    [SerializeField] private EnemyCopter goblinCopter;
     private int dmg;
     private void OnTriggerEnter(Collider other)
     {
@@ -14,7 +15,8 @@ public class HitBox : MonoBehaviour
             {
                 dmg = bullet.DealDmg();
                 if(isHead) dmg *= 2;
-                goblin.TakeDamage(dmg);
+                if(goblin != null)goblin.TakeDamage(dmg);
+                if(goblinCopter != null)goblinCopter.TakeDamage(dmg);
             }
         }
     }
