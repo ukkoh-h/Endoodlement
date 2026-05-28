5cd5e8cd291def114711c55753763f2174ee7b7e
ukkoh-h <ukkohh@gmail.com>
2026-05-28 14:13:31 +0300

    Enemy manager started + other enemy updates

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
